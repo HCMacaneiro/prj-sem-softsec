ab2df1b7a6f91831b1633a9df6dadb9b625bea73
Henrique Cardoso Macaneiro <hcmacaneiro@gmail.com>
2024-06-01 13:23:45 -0300

    Cria MenuInicialView e Controller

--- prj-sem-softsec/src/View/MenuInicialView.java
+++ prj-sem-softsec/src/View/MenuInicialView.java
@@ -1,4 +1,23 @@
 package View;
 
+import java.util.Scanner;
+
 public class MenuInicialView {
+
+    public void displayMenu() {
+        System.out.println("=====================");
+        System.out.println("Servidor de e-mail seguro");
+        System.out.println("=====================");
+        System.out.println("1. Login");
+        System.out.println("2. Cadastrar");
+        System.out.println("3. Sair");
+    }
+
+    public int getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite sua opção: ");
+        return scanner.nextInt();
+    }
 }
+
+
