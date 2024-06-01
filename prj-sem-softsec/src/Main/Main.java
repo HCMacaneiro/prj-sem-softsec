92fa94836383027a3539216b02121d6b14d2f9af
Henrique Cardoso Macaneiro <hcmacaneiro@gmail.com>
2024-06-01 14:23:35 -0300

    Inicia controller na Main

--- prj-sem-softsec/src/Main/Main.java
+++ prj-sem-softsec/src/Main/Main.java
@@ -1,9 +1,9 @@
 package Main;
 
+import Controller.MenuInicialController;
+
 public class Main {
     public static void main(String[] args) {
-
-
-
+        new MenuInicialController().handleMenu();
     }
 }
