084c5b336f998feb278b7cc824a83c442d232ad4
Henrique Maçaneiro <hcmacaneiro@gmail.com>
2024-06-04 22:10:27 -0300

    Adiciona comentarios ao codigo

--- prj-sem-softsec/src/Model/Authenticate.java
+++ prj-sem-softsec/src/Model/Authenticate.java
@@ -9,19 +9,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Authenticate {
+    // Informações AWS Cognito
     private static String USER_POOL_ID = "us-east-2_vSmEHFF6q";
     private static String CLIENT_ID = "7c7lbnjf8htdiasj52p2uh6q6a";
+    // Instancia variaveis que recebem respostas do Cognito
     private InitiateAuthResult authResponse;
     private AuthenticationResultType authResult;
+
     public Authenticate(String email, String senha) {
         AWSCognitoIdentityProvider awsc;
         try {
+            // Verificação de formato de e-mail com regex
             if (!isValidEmail(email)) {
                 throw new IllegalArgumentException("Formato de e-mail inválido.");
             }
             awsc = AWSCognitoIdentityProviderClientBuilder.standard()
                     .withRegion(Regions.US_EAST_2)
                     .build();
+        // Captura erros de autenticacao e com o Cognito
         } catch (IllegalArgumentException e) {
             System.out.println("Erro: " + e.getMessage());
             return;
@@ -49,10 +54,12 @@ public class Authenticate {
         }
     }
 
+    // Retorna o resultado da autenticação
     public boolean authenticate() {
         return authResult != null;
     }
 
+    // Função que verifica formato do e-mail
     private static boolean isValidEmail(String email) {
         String regex = "^[\\w!#$%&'*+/=?^`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[\\w](?:[\\w-]*[\\w])?$";
         return email.matches(regex);
--- prj-sem-softsec/src/Model/SignUp.java
+++ prj-sem-softsec/src/Model/SignUp.java
@@ -8,19 +8,23 @@ import com.amazonaws.services.cognitoidp.model.SignUpRequest;
 import com.amazonaws.services.cognitoidp.model.SignUpResult;
 
 public class SignUp {
+    // Informações AWS Cognito
     private String USER_POOL_ID = "us-east-2_vSmEHFF6q";
     private String CLIENT_ID = "7c7lbnjf8htdiasj52p2uh6q6a";
+    // Instancia variavel que receberá o resultado do cadastro
     private SignUpResult result;
 
     public SignUp(String email, String senha) {
         AWSCognitoIdentityProvider awsc;
         try {
+            // Verificação de formato de e-mail com regex
             if (!isValidEmail(email)) {
                 throw new IllegalArgumentException("Formato de e-mail inválido.");
             }
             awsc = AWSCognitoIdentityProviderClientBuilder.standard()
                     .withRegion(Regions.US_EAST_2)
                     .build();
+        // Captura erros de cadastro e com o Cognito
         } catch (IllegalArgumentException e) {
             System.out.println("Erro: " + e.getMessage());
             return;
@@ -43,10 +47,12 @@ public class SignUp {
         }
     }
 
+    // Retorna o resultado do cadastro
     public boolean signUp() {
         return result != null;
     }
 
+    // Função que verifica formato do e-mail
     private static boolean isValidEmail(String email) {
         String regex = "^[\\w!#$%&'*+/=?^`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[\\w](?:[\\w-]*[\\w])?$";
         return email.matches(regex);
